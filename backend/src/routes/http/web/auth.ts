e87faf9ee37fbd5a1c627c3715a58a7d5c8d8f1e
Khanh Tran <khanhtranngoccva@gmail.com>
2024-02-15 14:38:44 +0700

    fix: Cross-domain cookie tunnelling

--- backend/src/routes/http/web/auth.ts
+++ backend/src/routes/http/web/auth.ts
@@ -7,7 +7,7 @@ const webAuthRouter = express.Router();
 
 webAuthRouter.get("/google", passport.authenticate("google"));
 webAuthRouter.get("/google/callback", passport.authenticate("google", {
-  successRedirect: envHelper.get("FRONTEND_URL")
+  successRedirect: "/web-login"
 }));
 webAuthRouter.get("/logout", AuthController.http.signOut);
 webAuthRouter.get("/current-user", AuthController.http.getCurrentUser);
